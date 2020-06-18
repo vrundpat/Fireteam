5443cf714dcf74747e5a25c7e9ede9c106d7f8cc
Vrund Patel <vrundpatel@vrunds-mbp.home>
2020-06-18 17:57:29 -0400

    Further refined fireteam view layout

--- client/src/components/Fireteam/Fireteam.js
+++ client/src/components/Fireteam/Fireteam.js
@@ -11,7 +11,20 @@ export default class Fireteam extends Component {
         return (
             <div className="fireteam-root">
                 <div className="activity-type-image"></div>
-                <div className="fireteam-info"></div>
+                <div className="fireteam-info">
+                    <div className="fireteam-info-left-column">
+                        <h5>Join Button</h5>
+                        <h5>Leader</h5>
+                        <h5>Platform</h5>
+                        <h5>Activity Type</h5>
+                        <h5>Capacity</h5>
+                        <h5>Time Created</h5>
+                    </div>
+                    <div className="fireteam-info-right-column">
+                        <h5>Description</h5>
+                        <h5>Members full list with powers</h5>
+                    </div>
+                </div>
             </div>
         )
     }
